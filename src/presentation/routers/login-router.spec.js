5528b298ffa8b51e55ddefa5567648eeaf432713
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-03 18:02:12 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -3,18 +3,18 @@ const MissingParamError = require('../helpers/missing-param-error')
 const UnauthorizedError = require('../helpers/unauthorized-error')
 
 const makeSut = () => {
-  class SpyAuthUseCase {
+  class AuthUseCaseSpy {
     auth (email, password) {
       this.email = email
       this.password = password
     }
   }
 
-  const spyAuthUseCase = new SpyAuthUseCase()
-  const sut = new LoginRouter(spyAuthUseCase)
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const sut = new LoginRouter(authUseCaseSpy)
 
   return {
-    sut, spyAuthUseCase
+    sut, authUseCaseSpy
   }
 }
 
@@ -65,7 +65,7 @@ describe('Login Router', () => {
   })
 
   test('Should call AuthUseCase with correct params', () => {
-    const { sut, spyAuthUseCase } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
 
     const httpRequest = {
       body: {
@@ -75,8 +75,8 @@ describe('Login Router', () => {
     }
     sut.route(httpRequest)
 
-    expect(spyAuthUseCase.email).toBe(httpRequest.body.email)
-    expect(spyAuthUseCase.password).toBe(httpRequest.body.password)
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 
   test('Should return 401 when invalid credentials are provided', () => {
@@ -92,5 +92,35 @@ describe('Login Router', () => {
 
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
+  })
+
+  test('Should return 500 if no authUseCase is provided', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+  test('Should return 500 if authUseCase has no auth method', () => {
+    class AuthUseCaseSpy { }
+
+    const authUseCaseSpy = new AuthUseCaseSpy()
+    const sut = new LoginRouter(authUseCaseSpy)
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
