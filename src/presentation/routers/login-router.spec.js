d743e56e2386c5c959aacff61eb95af329566c40
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-03 17:31:03 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -77,4 +77,18 @@ describe('Login Router', () => {
     expect(spyAuthUseCase.email).toBe(httpRequest.body.email)
     expect(spyAuthUseCase.password).toBe(httpRequest.body.password)
   })
+
+  test('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com',
+        password: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(401)
+  })
 })
