c33a983d30b6c968a48a9f0dae5409bcde1c2923
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-05 13:10:08 -0300

    ensure TokenGenerator call JWT with correct values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,7 +1,11 @@
 module.exports = {
+  id: '',
+  secret: '',
   token: 'any_token',
 
   sign (id, secret) {
+    this.id = id
+    this.secret = secret
     return this.token
   }
 }
--- src/util/token-generator.spec.js
+++ src/util/token-generator.spec.js
@@ -2,11 +2,15 @@ const jwt = require('jsonwebtoken')
 
 const makeTokenGenerator = () => {
   class TokenGeneratorSpy {
+    constructor (secret) {
+      this.secret = secret
+    }
+
     async generate (id) {
-      return jwt.sign(id, 'secret')
+      return jwt.sign(id, this.secret)
     }
   }
-  const tokenGeneratorSpy = new TokenGeneratorSpy()
+  const tokenGeneratorSpy = new TokenGeneratorSpy('any_secret')
 
   return tokenGeneratorSpy
 }
@@ -35,5 +39,14 @@ describe('Token Generator', () => {
     const token = await sut.generate('any_id')
 
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const { sut } = makeSut()
+
+    await sut.generate('any_id')
+
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
