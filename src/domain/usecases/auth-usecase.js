5e13ea04b0966552ad3cf2f7b1379171b628490e
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-04 22:49:53 -0300

    simplify code on AuthUseCase

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -25,18 +25,13 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email)
+    const isValid = user && await this.encrypter.compare(password, user.password)
 
-    if (!user) {
-      return null
+    if (isValid) {
+      const token = await this.tokenGenerator.generate(user.id)
+      return token
     }
 
-    const isValid = await this.encrypter.compare(password, user.password)
-
-    if (!isValid) {
-      return null
-    }
-
-    const token = await this.tokenGenerator.generate(user.id)
-    return token
+    return null
   }
 }
