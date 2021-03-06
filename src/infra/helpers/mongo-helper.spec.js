2777ceb06e67aad422241814e714e5bbdeff3adf
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-06 15:03:18 -0300

    disconnect mongodb after mongo helper test

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,15 +1,15 @@
-const MongoHelper = require('./mongo-helper')
+const sut = require('./mongo-helper')
 
 describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
   afterAll(async () => {
-    MongoHelper.disconnect()
+    await sut.disconnect()
   })
 
   test('Should reconnect when getDb() is invoked and client is disconnected', async () => {
-    const sut = MongoHelper
-
-    await sut.connect(process.env.MONGO_URL)
-
     expect(sut.db).toBeTruthy()
     await sut.disconnect()
     expect(sut.db).toBeFalsy()
