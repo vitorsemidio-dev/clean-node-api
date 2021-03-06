40757a33151bd16a8cbba0307c3955bae098794b
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-03-06 14:20:13 -0300

    ensure requests return content-type JSON as default

---
+++ src/main/middlewares/content-type.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+
+const app = require('../config/app')
+
+describe('Content-Type Middleware', () => {
+  test('Should returns json content-type as default', async () => {
+    app.get('/test-content-type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test-content-type')
+      .expect('content-type', /json/)
+  })
+})
